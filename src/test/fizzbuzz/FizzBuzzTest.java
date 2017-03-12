0a3635197c7516ea57dd9a61840f60754580e338
Heinz Karsten Hiekmann <k.hiekmann@andagon.com>
2017-03-12 18:55:10 +0100

    green - init

---
+++ src/test/fizzbuzz/FizzBuzzTest.java
@@ -0,0 +1,21 @@
+package fizzbuzz;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+
+/**
+ * Created by HiekmaHe on 12.03.2017.
+ *
+ * http://ccd-school.de/coding-dojo/function-katas/fizzbuzz/
+ *
+ */
+public class FizzBuzzTest
+{
+
+	@Test
+	public void sanity() {
+		assertTrue(true);
+	}
+}
