4cf7269435a5913b5ed07040cfd357d70480d892
Heinz Karsten Hiekmann <k.hiekmann@andagon.com>
2017-03-12 19:48:59 +0100

    green - added test cases. It occured to me, that multiples have not been tested

--- src/test/fizzbuzz/FizzBuzzTest.java
+++ src/test/fizzbuzz/FizzBuzzTest.java
@@ -44,6 +44,28 @@ public class FizzBuzzTest
 	}
 
 	@Test
+	public void givenBuzzWhen15ThenBuzz() throws FizzException
+	{
+		// arrange
+		Fizz buzz = new Fizz(5, "Buzz");
+
+		// act
+		// assert
+		assertException(buzz, 15);
+	}
+
+	@Test
+	public void givenBuzzWhen10ThenBuzz() throws FizzException
+	{
+		// arrange
+		Fizz buzz = new Fizz(5, "Buzz");
+
+		// act
+		// assert
+		assertException(buzz, 10);
+	}
+
+	@Test
 	public void givenBuzzWhen5ThenBuzz() throws FizzException
 	{
 		// arrange
@@ -52,6 +74,19 @@ public class FizzBuzzTest
 		// act
 		// assert
 		assertException(buzz, 5);
+	}
+
+	@Test
+	public void givenBuzzWhen2Then1() throws FizzException
+	{
+		// arrange
+		Fizz buzz = new Fizz(5, "Buzz");
+
+		// act
+		String response = buzz.respondTo(2);
+
+		// assert
+		assertEquals(String.valueOf(2), response);
 	}
 
 	@Test
@@ -68,6 +103,19 @@ public class FizzBuzzTest
 	}
 
 	@Test
+	public void givenFizzWhen2Then2() throws FizzException
+	{
+		// arrange
+		Fizz fizz = new Fizz(3, "Fizz");
+
+		// act
+		String response = fizz.respondTo(2);
+
+		// assert
+		assertEquals(String.valueOf(2), response);
+	}
+
+	@Test
 	public void givenFizzWhen1Then1() throws FizzException
 	{
 		// arrange
@@ -81,13 +129,34 @@ public class FizzBuzzTest
 	}
 
 	@Test
+	public void givenFizzWhen9ThenFizz() throws FizzException
+	{
+		// arrange
+		Fizz fizz = new Fizz(3, "Fizz");
+
+		// act
+		// assert
+		assertException(fizz, 9);
+	}
+
+	@Test
+	public void givenFizzWhen6ThenFizz() throws FizzException
+	{
+		// arrange
+		Fizz fizz = new Fizz(3, "Fizz");
+
+		// act
+		// assert
+		assertException(fizz, 6);
+	}
+
+	@Test
 	public void givenFizzWhen3ThenFizz() throws FizzException
 	{
 		// arrange
 				Fizz fizz = new Fizz(3, "Fizz");
 
 		// act
-
 		// assert
 		assertException(fizz, 3);
 	}
